5be6ed5923de9273592a2c8ef568d9f8b9953a85
ovenone <1162555170@qq.com>
2021-12-05 14:52:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
 data = {'result': 'this is a test'}
-host = ('10.0.0.8', 888)
+host = ('0.0.0.0', 888)
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
 
 ## v2ray订阅地址
