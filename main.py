b4f8d132e019e8c78bfba4f7a3a70e4706df5b83
Huac233 <96987145+Huac233@users.noreply.github.com>
2022-05-12 18:12:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 data = {'result': 'this is a test'}
 host = ('0.0.0.0', 888)
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36'}
 
 ## v2ray订阅地址
 subscribe_url = ''
